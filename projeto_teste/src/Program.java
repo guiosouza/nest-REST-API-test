8a7144cb5e7129096fc1590123c24cba56121ebc
Guilherme <78989152+guiosouza@users.noreply.github.com>
2024-06-23 18:05:41 -0300

    iygyuib

---
+++ projeto_teste/src/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    public static void main(String[] args) {
+        String sex = "ASD";
+        int age = 67;
+
+        if(sex.equals("masculino") || age >= 65) {
+            System.out.println("APOSENTADO");
+        } else { 
+            System.out.println("NÃO É APOSENTADO");
+        }
+    }
+}
