29305f337116687c70110717ba03fd742ca472a5
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-10-03 11:36:31 -0300

    providers for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,52 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
-  findAll(@Query('role') role?: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND') {
-    return [];
+  findAll(
+    @Query('role')
+    role?: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS',
+  ) {
+    return this.usersService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id); // Unary plus to convert to number
   }
 
   @Post()
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdated: {
+      name: string;
+      email: string;
+      role: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdated);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,4 +3,91 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   
+  private users = [
+    {
+      id: 1,
+      name: 'Solid Snake',
+      email: 'snake@outerheaven.org',
+      role: 'FOX_HOUND',
+    },
+    {
+      id: 2,
+      name: 'Big Boss',
+      email: 'boss@outerheaven.org',
+      role: 'OUTER_HEAVEN',
+    },
+    {
+      id: 3,
+      name: 'Kazuhira Miller',
+      email: 'miller@diamond.dog',
+      role: 'DIAMONG_DOG',
+    },
+    {
+      id: 4,
+      name: 'Revolver Ocelot',
+      email: 'ocelot@foxhound.org',
+      role: 'FOX_HOUND',
+    },
+    {
+      id: 5,
+      name: 'Venom Snake',
+      email: 'venom@diamond.dog',
+      role: 'BIG_BOSS',
+    },
+  ];
+
+  findAll(role?: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'DIAMONG_DOG' | 'OUTER_HEAVEN' | 'FOX_HOUND' | 'BIG_BOSS';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
 }
